73638a15231db0414dab74ed17e104c37c9f0308
Martin Trapp <trapp.martin@gmail.com>
2019-07-18 15:43:38 +0200

    [WIP] Now using BLAS routine. Potentially faster...

--- src/AdvancedCholeskey.jl
+++ src/AdvancedCholeskey.jl
@@ -1,54 +1,152 @@
+"""
+    AdvancedCholeskey module
+
+This module aims to implement extensions to existing Cholesky factorisations
+available in Julia. 
+
+Contains:
+* `chol_continue!`: Continue a partial Cholesky factorisation.
+* `test1`: Test for `chol_continue!`
+* `rightlooking_cholesky!`: Right-looking Cholesky factorization (WIP).
+
+"""
 module AdvancedCholeskey
 
 using LinearAlgebra
 using BenchmarkTools
 
-# A = Float64.([25 15 5; 15 18 0; -5 0 11])
-# a = A[1:2,1:2]
+export genCov, test, rightlooking_cholesky!
 
-function test(A, a)
-    _A = deepcopy(A)
-    _a = deepcopy(a)
+# Helper function
+genCov(D::Int) = Symmetric(rand(D,D) .+ Matrix(I*D,D,D))
 
-    t1 = @elapsed begin
-        LinearAlgebra.cholesky(Symmetric(_a, :U))
-        LinearAlgebra.cholesky(Symmetric(_A, :U))
+"""
+Right-looking blocked Cholesky factorization.
+
+The right-looking Cholesky fact. favors parallelism over data locality
+by quickly exposing a large volume of work. At the same time, it modifies
+the entire trailing submatrix.
+
+1. POTRF (current triangle)
+2. TRSM (sub-matrix below current triangle)
+3. SYRK (trailing triangle)
+
+WIP!
+"""
+function rightlooking_cholesky!(A::Symmetric{T,Array{T,2}}, 
+                                start::Int, 
+                                stop::Int; 
+                                ispotrf::Bool = false) where {T<:LinearAlgebra.BlasFloat}
+    e = size(A,1)
+    if ispotrf 
+        _, info = LAPACK.potrf!('L', view(A.data, start:stop, start:stop))
+        @assert info == zero(info)
     end
 
-    t2 = @elapsed res = begin
-        U = LinearAlgebra.cholesky(Symmetric(a, :U))
+    if e > stop
+        L = tril(A[start:stop, start:stop])
+        BLAS.trsm!('L','U','N','N',one(T),L, view(A.data, stop+1:e, start:stop))
 
-        @inbounds begin
-            for i in 1:size(a,2)
-                for j in 1:i
-                    A[j,i] = U.U[j,i]
-                    A[i,j] = U.U[j,i]
-                end
-            end
-        end
-        chol2 = chol_continue!(A, UpperTriangular, 3)
     end
-
-    @assert t1 > t2
-
-    return t1 - t2, res
+    A
 end
 
-function chol_continue!(A::AbstractMatrix, ::Type{UpperTriangular}, ki::Int)
+function test2()
+    D = 6
+    P = 3
+
+    Σ = genCov(D)
+    A = deepcopy(Σ)
+    B = deepcopy(Σ)
+
+    LAPACK.potrf!('U', view(A.data, 1:P, 1:P))
+    LAPACK.potrf!('U', view(B.data, 1:P, 1:P))
+
+    ki = P+1
+    n = D
+    t1 = @elapsed begin
+    @inbounds begin
+        for k = 1:ki-1
+            AkkInv = inv(copy(A.data[k,k]'))
+            for j in ki:n
+                @simd for i = 1:k-1
+                    A.data[k,j] -= A.data[i,k]'A.data[i,j]
+                end
+                A.data[k,j] = AkkInv*A.data[k,j]
+            end
+        end
+    end
+    end
+
+    t2 = @elapsed BLAS.trsm!('L','U','T','N',1.0, triu(view(B.data, 1:P,1:P)), view(B.data, 1:P, ki:n))
+
+    return A, B, t1, t2
+end
+
+"""
+Run a simple test on `chol_continue!`.
+
+Return:
+* `t1`: time taken for LinearAlgebra.cholesky!
+* `t2`: time taken for LAPACK.potrf! and chol_continue!
+* `Δ`: difference between Cholesky factorisations
+"""
+function test1(;useBLAS=true)
+    D = 50
+    P = 10
+    Σ = genCov(D)
+    U = deepcopy(Σ)
+    A = deepcopy(Σ)
+
+    t1 = @elapsed LinearAlgebra.cholesky!(U)
+    t2 = @elapsed begin
+        LAPACK.potrf!('U', view(A.data, 1:P, 1:P))
+        chol_continue!(A.data, UpperTriangular, P+1; useBLAS = useBLAS)
+    end
+
+    return t1, t2, sum(abs.(UpperTriangular(A) .- UpperTriangular(U)))
+end
+
+"""
+# Continue a Cholesky decomposition of `A` from `ki` on.
+`chol_continue!(A::AbstractMatrix, ::Type{UpperTriangular}, ki::Int)`
+
+## Usage:
+
+```julia
+A = genCov(10)
+LAPACK.potrf!('U', view(A.data, 1:5, 1:5))
+chol_continue!(A.data, UpperTriangular, 5+1)
+```
+
+"""
+function chol_continue!(A::AbstractMatrix{T}, 
+                        ::Type{UpperTriangular}, 
+                        ki::Int; 
+                        useBLAS::Bool = true
+                        ) where {T<:LinearAlgebra.BlasFloat}
     @assert !LinearAlgebra.has_offset_axes(A)
     n = LinearAlgebra.checksquare(A)
     @assert ki <= n
 
-    @inbounds begin
-        for k = 1:ki-1
-            AkkInv = inv(copy(A[k,k]'))
-            for j in ki:n
-                @simd for i = 1:k-1
-                    A[k,j] -= A[i,k]'A[i,j]
+    if !useBLAS
+        # Native Julia version
+        @inbounds begin
+            for k = 1:ki-1
+                AkkInv = inv(copy(A[k,k]'))
+                for j in ki:n
+                    @simd for i = 1:k-1
+                        A[k,j] -= A[i,k]'A[i,j]
+                    end
+                    A[k,j] = AkkInv*A[k,j]
                 end
-                A[k,j] = AkkInv*A[k,j]
             end
         end
+    else
+        # BLAS version (faster)
+        P = ki-1
+        triu!(view(A, 1:P, 1:P))
+        BLAS.trsm!('L','U','T','N',one(T), view(A, 1:P, 1:P), view(A, 1:P, ki:n))
     end
 
     @inbounds begin
